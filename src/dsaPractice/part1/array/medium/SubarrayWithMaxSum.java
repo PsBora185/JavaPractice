27058a6dd0c0143f979775d3abe5dd06313ac86e
pranav <pranavsinghbora@gmail.com>
2025-08-09 17:25:28 +0530

    Subarray eith max sum Optimal solution

--- src/dsaPractice/part1/array/medium/SubarrayWithMaxSum.java
+++ src/dsaPractice/part1/array/medium/SubarrayWithMaxSum.java
@@ -19,33 +19,6 @@ public class SubarrayWithMaxSum {
             }
         }
 
-        public static void find(int[] nums) {
-
-            int tst = 0, st = 0, end = 0;
-            int sum = nums[0], maxSum = nums[0];
-
-            for (int i = 0; i < nums.length; i++) {
-
-                // keep track of current sum
-                if (sum + nums[i] < 0){ // when current sum is in minus no point in taking it for subarray , it will reduce value
-                    sum = 0;  // so make it 0 , we will take subarray with +ve sum
-                    tst = i+1; // and keeping the starting idx for that new subarray
-                } else
-                    sum += nums[i];
-
-                if (maxSum < sum){  // when current sum is greater than max sum
-                    maxSum = sum; // make it  new max sum
-                    st =  tst; // now we will take the starting idx of new subarray cuz its sum is max
-                    end = i; // end will update if we are taking more element in contiguous subarray
-                }
-            }
-
-            System.out.println("Maximum sum is : "+maxSum);
-            for (int i = st; i <= end; i++) {
-                System.out.print(nums[i] + " ");
-            }
-        }
-
         for (int i = st; i <= end; i++) {
             System.out.print(nums[i] + " ");
         }
